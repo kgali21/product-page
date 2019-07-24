1a4f03eee5a570b1a1ee53ae0b677ef4f35ae241
Kamran Ali <kgali21@gmail.com>
2019-07-24 10:37:50 -0700

    Final turn in

--- src/shopping-cart.js
+++ src/shopping-cart.js
@@ -3,18 +3,26 @@ import techs from './data/tech.js';
 import { findProduct, calcOrderTotal } from './register.js';
 import compileLineItem from './compile-line-item.js';
 import { toUSD } from './format.js';
+import shoppingCart from './data/order.js';
 
 const tbody = document.querySelector('tbody');
 
-for(let i = 0; i < order.length; i++) {
-    const lineItem = order[i];
-    const tech = findProduct(techs, lineItem.code);
-    const dom = compileLineItem(lineItem, tech);
-
-    tbody.appendChild(dom);
-}
+listItems();
 // calc order total
 
+const totalCell = document.getElementById('order-total-cell');
+
+const orderTotal = toUSD(calcOrderTotal(shoppingCart, techs));
+totalCell.textContent = orderTotal;
     
 
+
+function listItems() {
+    for (let i = 0; i < order.length; i++) {
+        const lineItem = order[i];
+        const tech = findProduct(techs, lineItem.code);
+        const dom = compileLineItem(lineItem, tech);
+        tbody.appendChild(dom);
+    }
+}
 // assign to total cell
